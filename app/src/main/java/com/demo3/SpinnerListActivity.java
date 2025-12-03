60947c0df8ccc7574bb220994495963b70fe14f6
jeel-work-mob-06 <jeelparekh2024@gmail.com>
2025-12-03 19:22:27 +0530

    Spinner object

---
+++ app/src/main/java/com/demo3/SpinnerListActivity.java
@@ -0,0 +1,22 @@
+package com.demo3;
+
+import android.os.Bundle;
+import android.widget.Spinner;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class SpinnerListActivity extends AppCompatActivity {
+
+    Spinner spinner; // variable for spinner
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_spinner_list);
+        spinner = findViewById(R.id.spinner);
+    }
+}
