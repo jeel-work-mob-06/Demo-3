dea80a602b2c6f654b92fb8d95a1c5f08f6dc526
jeel-work-mob-06 <jeelparekh2024@gmail.com>
2025-12-05 16:48:04 +0530

    Cudtome Recycler View with ArrayList

---
+++ app/src/main/java/com/demo3/RecyclerSecondAdapter.java
@@ -0,0 +1,63 @@
+package com.demo3;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.bumptech.glide.Glide;
+
+import java.util.ArrayList;
+
+public class RecyclerSecondAdapter extends RecyclerView.Adapter<RecyclerSecondAdapter.MyHolder> {
+
+    Context context;
+    ArrayList<CustomList> arrayList;
+    public RecyclerSecondAdapter(Context context, ArrayList<CustomList> arrayList) {
+        this.context = context;
+        this.arrayList = arrayList;
+
+
+    }
+
+    @NonNull
+    @Override
+    public MyHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.custom_recycler,parent,false);
+        return new MyHolder(view);
+    }
+
+    public class MyHolder extends RecyclerView.ViewHolder {
+
+        ImageView imageView;
+        TextView title,offer,desc;
+        public MyHolder(@NonNull View itemView) {
+            super(itemView);
+            title = itemView.findViewById(R.id.cust_recycler_title);
+            offer = itemView.findViewById(R.id.cust_recycler_offer);
+            desc = itemView.findViewById(R.id.cust_recycler_desc);
+            imageView = itemView.findViewById(R.id.cust_recycler_image);
+        }
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull MyHolder holder, int position) {
+
+        holder.title.setText(arrayList.get(position).getTitle());
+        holder.offer.setText(arrayList.get(position).getOffer());
+        holder.desc.setText(arrayList.get(position).getDec());
+        Glide.with(context).asGif().load(arrayList.get(position).getImage()).placeholder(R.mipmap.ic_launcher).into(holder.imageView);
+    }
+
+    @Override
+    public int getItemCount() {
+        return 0;
+    }
+
+
+}
